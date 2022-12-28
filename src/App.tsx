91813290503205efe937ff4347934a46c2c73666
Shalin Scupham <shalin.scupham@gmail.com>
2022-12-28 00:09:29 -0500

    Hello data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
+import Papa from 'papaparse';
 
-const App = () => {
-  return (
+const App =  async () => {
+    const data = Papa.parse('./thefiles.csv', {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: ({ data }) => {
+            console.log(data)
+        }
+    })
+
+    return (
       <h1 className="text-3xl font-bold underline">
           Hello world!
       </h1>
