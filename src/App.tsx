8719935b715ff2b92be1b5e5f158684cd7999dab
Shalin Scupham <shalin.scupham@gmail.com>
2023-03-27 22:46:37 -0400

    even fancier searching, thanks bito

--- src/App.tsx
+++ src/App.tsx
@@ -11,40 +11,66 @@ const App = () => {
   const [files, setFiles] = useState([] as any)
   const [searchParams, setSearchParams] = useSearchParams()
   const query = searchParams.get('query') || ''
+  const searchWords = query.toLowerCase().split(" ");
   const pageNumber = parseInt(searchParams.get('page') || '0')
 
   useEffect(() => {
-    const tightFilter = filter(data, (file: any) => {
-        const fileName = file['File Code']
-        if (!fileName) return false
-        if (!query) return true
-      return query.split(' ').every((searchString:string ) => (
-          fileName.toLowerCase().split(" ").some((word:string) => (
-              word.startsWith(searchString.toLowerCase().trim()))
-          )))
-    })
-
-    const looseFilter = filter(data, (file: any) => {
-      const fileName = file['File Code']
-      if (!fileName) return false
-      if (!query) return true
-      return query.split(' ').some((searchString:string ) => (
-              fileName.toLowerCase().split(" ").some((word:string) => (
-                  word.startsWith(searchString.toLowerCase().trim()))
-              )))
-    })
-
-    const loosestFilter = filter(data, (file: any) => {
-      const fileName = file['File Code']
-      if (!fileName) return false
-      if (!query) return true
-      return query.split(' ').some((searchString:string ) => (
-          fileName.toLowerCase().split(" ").some((word:string) => (
-              word.includes(searchString.toLowerCase().trim()))
-          )))
-    })
-    setFiles(tightFilter.length > 1 ? tightFilter : looseFilter.length > 0 ? looseFilter: loosestFilter)
-  }, [data, query])
+      const combinedFilter = data.filter((file: any) => {
+          const fileName = file['File Code'];
+          if (!fileName) return false;
+          if (!query) return true;
+          const fileNameWords = fileName.toLowerCase().split(" ");
+          // Check for tight filter matches
+          if (
+              searchWords.every((searchString:string ) => (
+                  fileNameWords.includes(searchString.toLowerCase().trim()))
+              )
+          ) {
+              return true;
+          }
+          // Check for loose filter matches
+          if (
+              searchWords.some((searchString:string ) => (
+                  fileNameWords.some((word:string) => (
+                      word.startsWith(searchString.toLowerCase().trim()))
+                  ))
+              )
+          ) {
+              return true;
+          }
+          // Check for loosest filter matches
+          if (
+              searchWords.some((searchString:string ) => (
+                  fileNameWords.some((word:string) => (
+                      word.includes(searchString.toLowerCase().trim()))
+                  ))
+              )
+          ) {
+              return true;
+          }
+          return false;
+      });
+      // Sort the results to prioritize tight filter matches
+      const sortedFiles = combinedFilter.sort((file1: any, file2: any) => {
+          const file1NameWords = file1['File Code'].toLowerCase().split(" ");
+          const file2NameWords = file2['File Code'].toLowerCase().split(" ");
+          // Check if file1 matches the tight filter
+          const file1TightMatch = searchWords.every((searchString:string ) => (
+              file1NameWords.includes(searchString.toLowerCase().trim()))
+          );
+          // Check if file2 matches the tight filter
+          const file2TightMatch = searchWords.every((searchString:string ) => (
+              file2NameWords.includes(searchString.toLowerCase().trim()))
+          );
+          if (file1TightMatch && !file2TightMatch) {
+              return -1;
+          }
+          if (!file1TightMatch && file2TightMatch) {
+              return 1;
+          }
+          return 0;
+      });
+      setFiles(sortedFiles);  }, [data, query])
 
   const setQuery = (string: string) => {
     setSearchParams({ query: string.toLowerCase().replace('*', ''), page: '0' })
