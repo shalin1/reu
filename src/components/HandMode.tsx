f188c91ac24cb40b7698ab2f1a14a0a8e61718dd
Shalin Scupham <shalin.scupham@gmail.com>
2023-04-08 19:03:03 -0400

    deal with belief and facilitations in general

--- src/components/HandMode.tsx
+++ src/components/HandMode.tsx
@@ -9,12 +9,12 @@ const HandMode: React.FC<Props> = ({ name }) => {
     const sanitized = name.trim().toLowerCase()
     const sanitizedWithoutAst = sanitized.replace('*', '')
     const sanitizedWithoutColon = sanitizedWithoutAst.replace(':', '')
-    const path = `/src/images/${sanitizedWithoutColon}.png`
-    console.log('*' + path + '*')
+    const santizedWithoutFacilitation = sanitizedWithoutColon.replace(' facilitation', '')
+    const dealWithBelief = santizedWithoutFacilitation.startsWith('belief') ? 'belief' : santizedWithoutFacilitation
+    const path = `/src/images/${dealWithBelief}.png`
+    console.log('| ' + path + ' |')
     const modules = import.meta.glob('/src/images/*', { eager: true })
-    console.log(modules)
     const mod = modules[path] as { default: string }
-    console.log(mod)
     return mod?.default
   }
 
