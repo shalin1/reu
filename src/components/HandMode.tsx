740220912ebc0799d43b2d98aedd13dd25a8833f
Shalin Scupham <shalin.scupham@gmail.com>
2023-04-08 18:58:42 -0400

    reckless image edits

--- src/components/HandMode.tsx
+++ src/components/HandMode.tsx
@@ -6,7 +6,10 @@ interface Props {
 
 const HandMode: React.FC<Props> = ({ name }) => {
   const getAssetSrc = (name: string) => {
-    const path = `/src/images/${name.trim().toLowerCase()}.png`
+    const sanitized = name.trim().toLowerCase()
+    const sanitizedWithoutAst = sanitized.replace('*', '')
+    const sanitizedWithoutColon = sanitizedWithoutAst.replace(':', '')
+    const path = `/src/images/${sanitizedWithoutColon}.png`
     console.log('*' + path + '*')
     const modules = import.meta.glob('/src/images/*', { eager: true })
     console.log(modules)
