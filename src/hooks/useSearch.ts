117190f687579e0708d1d2f540c301d0f3d74986
Shalin Scupham <shalin.scupham@gmail.com>
2023-06-16 13:46:52 -0400

    Revert "more sorting logic"
    This reverts commit 60a01268e48278c12934f7d6119bbb9240e3af24.

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -24,33 +24,26 @@ const useSearch = ({ data }: SearchFilesParams) => {
       )
     })
 
-    // Group files by 'File Code'
-    const fileGroups = combinedFilter.reduce((groups: any, file: any) => {
-      const key = file['File Code']
-      if (!groups[key]) {
-        groups[key] = []
+    const sortedFiles = combinedFilter.sort((file1: any, file2: any) => {
+      const file1NameWords = file1['File Code'].toLowerCase().split(' ')
+      const file2NameWords = file2['File Code'].toLowerCase().split(' ')
+
+      const file1TightMatch = searchWords.every((searchString: string) =>
+        file1NameWords.some((word: string) => word.includes(`(${searchString.toLowerCase().trim()})`)),
+      )
+
+      const file2TightMatch = searchWords.every((searchString: string) =>
+        file2NameWords.some((word: string) => word.includes(`(${searchString.toLowerCase().trim()})`)),
+      )
+
+      if (file1TightMatch && !file2TightMatch) {
+        return -1
       }
-      groups[key].push(file)
-      return groups
-    }, {})
-
-    // Sort within each group
-    Object.keys(fileGroups).forEach((key) => {
-      fileGroups[key].sort((file1: any, file2: any) => {
-        const set1 = file1['Set#']
-        const set2 = file2['Set#']
-
-        if (set1 === 'F') return 1
-        if (set2 === 'F') return -1
-
-        return Number(set1) - Number(set2)
-      })
+      if (!file1TightMatch && file2TightMatch) {
+        return 1
+      }
+      return 0
     })
-
-    // Concatenate all groups together
-    const sortedFiles = Object.keys(fileGroups).reduce((result: any[], key: string) => {
-      return [...result, ...fileGroups[key]]
-    }, [])
 
     setFiles(sortedFiles)
   }, [data, query])
