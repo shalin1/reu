e4611f96db7865ded14d06daf1e5402d15aab1b6
Shalin Scupham <shalin.scupham@gmail.com>
2023-06-16 15:05:14 -0400

    more sorting

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -13,27 +13,33 @@ const useSearch = ({ data }: SearchFilesParams) => {
   const [files, setFiles] = useState([] as any)
 
   useEffect(() => {
-    const combinedFilter = data.filter((file: any) => {
+    const filteredFiles = data.filter((file: any) => {
       const fileName = file['File Code']
       if (!fileName) return false
       if (!query) return true
       const fileNameWords = fileName.toLowerCase().split(' ')
 
-      return searchWords.every((searchString: string) =>
-        fileNameWords.some((word: string) => word.includes(searchString.toLowerCase().trim())),
-      )
+      return searchWords.every((searchString: string) => {
+        const sanitizedString = searchString.replace(/\n/g, ' ').replace('*', '')
+        return fileNameWords.some((fileName: string) => {
+          if (fileName[0] === '(') {
+            return fileName.includes(sanitizedString.toLowerCase().trim())
+          }
+          return fileName.startsWith(sanitizedString.toLowerCase().trim())
+        })
+      })
     })
 
-    const sortedFiles = combinedFilter.sort((file1: any, file2: any) => {
+    const sortedFiles = filteredFiles.sort((file1: any, file2: any) => {
       const file1NameWords = file1['File Code'].toLowerCase().split(' ')
       const file2NameWords = file2['File Code'].toLowerCase().split(' ')
 
       const file1TightMatch = searchWords.every((searchString: string) =>
-        file1NameWords.some((word: string) => word.includes(`(${searchString.toLowerCase().trim()})`)),
+        file1NameWords.some((word: string) => word === `(${searchString.toLowerCase().trim()})`),
       )
 
       const file2TightMatch = searchWords.every((searchString: string) =>
-        file2NameWords.some((word: string) => word.includes(`(${searchString.toLowerCase().trim()})`)),
+        file2NameWords.some((word: string) => word === `(${searchString.toLowerCase().trim()})`),
       )
 
       if (file1TightMatch && !file2TightMatch) {
@@ -42,6 +48,33 @@ const useSearch = ({ data }: SearchFilesParams) => {
       if (!file1TightMatch && file2TightMatch) {
         return 1
       }
+
+      const file1FullWordMatch = searchWords.every((searchString: string) =>
+        file1NameWords.some((word: string) => word === searchString.toLowerCase().trim()),
+      )
+
+      const file2FullWordMatch = searchWords.every((searchString: string) =>
+        file2NameWords.some((word: string) => word === searchString.toLowerCase().trim()),
+      )
+
+      if (file1FullWordMatch && !file2FullWordMatch) {
+        return -1
+      }
+      if (!file1FullWordMatch && file2FullWordMatch) {
+        return 1
+      }
+
+      const sanitizeForSort = (fileName: string) => {
+        let sanitized = fileName.toLowerCase().replace(/\*$/, '') // Remove trailing *
+        sanitized = sanitized.replace(/\bfacilitation\b/, '') // Remove 'facilitation'
+        return sanitized.trim() // Remove leading and trailing spaces
+      }
+      if (file1['File Code'] === file2['File Code']) {
+        const setNumber1 = file1['Set#'] === 'F' ? Infinity : parseInt(file1['Set#'], 10)
+        const setNumber2 = file2['Set#'] === 'F' ? Infinity : parseInt(file2['Set#'], 10)
+        return setNumber1 - setNumber2
+      }
+
       return 0
     })
 
