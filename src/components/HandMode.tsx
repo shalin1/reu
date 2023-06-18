ab1bf81308df70a6cd3ce8783584614606cf104b
Shalin Scupham <shalin.scupham@gmail.com>
2023-06-18 11:26:15 -0400

    refactor (thnx chatgpt)

--- src/components/HandMode.tsx
+++ src/components/HandMode.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
+
 interface Props {
   name: string
 }
 
 const HandMode: React.FC<Props> = ({ name }) => {
-  const getAssetSrc = (name: string) => {
+  const sanitizeName = (name: string) => {
     const afterColon = name.split(':')[1] || name
-    const withoutParentheses = afterColon.replace(/\s*\(.*?\)\s*/g, '')
-    const sanitized = withoutParentheses.trim().toLowerCase()
-    const sanitizedWithoutAst = sanitized.replace('*', '')
-    const sanitizedWithoutQuote = sanitizedWithoutAst.replace('’', '')
-    const sanitizedWithoutFacilitation = sanitizedWithoutQuote.replace(' facilitation', '')
-    const dealWithBelief = sanitizedWithoutFacilitation.startsWith('belief') ? 'belief' : sanitizedWithoutFacilitation
-    const dealWithImplant = dealWithBelief.startsWith('implant') ? 'implant' : dealWithBelief
-    const dealWithNutrient = dealWithImplant.startsWith('nutrient') ? 'nutrient' : dealWithImplant
-    const dealWithHelpmode = dealWithNutrient.replace(' helpmode', '')
-    const path = `/src/images/${dealWithHelpmode}.png`
+    const sanitized = afterColon
+      .replace(/\s*\(.*?\)\s*/g, '')
+      .trim()
+      .toLowerCase()
+    return sanitized.replace(/[*’]| facilitation| helpmode/g, '')
+  }
+
+  const checkForPrefixes = (name: string) => {
+    const prefixes = ['belief', 'implant', 'nutrient']
+    return prefixes.some((prefix) => name.startsWith(prefix))
+      ? prefixes.find((prefix) => name.startsWith(prefix))
+      : name
+  }
+
+  const getAssetSrc = (name: string) => {
+    const sanitized = sanitizeName(name)
+    const finalName = checkForPrefixes(sanitized)
+    const path = `/src/images/${finalName}.png`
     const modules = import.meta.glob('/src/images/*', { eager: true })
     const mod = modules[path] as { default: string }
     return mod?.default
@@ -23,4 +32,5 @@ const HandMode: React.FC<Props> = ({ name }) => {
 
   return <img className="w-full" alt={name} src={getAssetSrc(name)} />
 }
+
 export default HandMode
