16ead9834b2f9393e2e263d7daba7ee8b836bc03
Shalin Scupham <shalin.scupham@gmail.com>
2023-07-09 20:06:43 -0400

    update title

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -8,7 +8,7 @@ const devOnlyPlugins = [getStartedPlugin()]
 
 export default defineConfig({
   name: 'default',
-  title: 'lavender-crane',
+  title: 'inri',
 
   projectId: 'rxwn78ar',
   dataset: 'production',
@@ -19,4 +19,3 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
-
