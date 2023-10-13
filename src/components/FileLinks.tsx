fa81c6e297db3662c40819a32c4667f245f8656a
Shalin Scupham <shalin.scupham@gmail.com>
2023-10-12 21:00:19 -0400

    another damn null guard

--- src/components/FileLinks.tsx
+++ src/components/FileLinks.tsx
@@ -28,7 +28,7 @@ const FileLinks: React.FC<Props> = ({ file, sanityFile, search }) => {
           {range(1, 9).map((linkNum) => {
             const filemakerSuffix = descriptionRows[quadrant - 1][linkNum - 1]
             const filemakerDescription = file && file[`sm${filemakerSuffix}`]
-            const sanityDescription = sanityFile[`q${quadrant}r${linkNum}`]
+            const sanityDescription = sanityFile && sanityFile[`q${quadrant}r${linkNum}`]
             const description = sanityDescription || filemakerDescription
             const rawLink = file && file[`Goto sm${nameRows[quadrant - 1][linkNum - 1]}`]
             const link = rawLink && rawLink.replace('Entry\rET', 'ET')
