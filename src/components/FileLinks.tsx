9c524db59577487101c3407625e3c248b1ff0794
Shalin Scupham <shalin.scupham@gmail.com>
2023-10-14 00:18:46 -0400

    More cleanup

--- src/components/FileLinks.tsx
+++ src/components/FileLinks.tsx
@@ -20,14 +20,14 @@ const LinkGrid = ({
 }) => {
   return (
     <a className="relative flex h-[51px] hover:cursor-pointer" onClick={onClick}>
-      <div className="flex w-[28px] items-center justify-center border-[1px] border-r-[1px] border-t-[0px] border-neutral-800 bg-[#c5f7c5]">
+      <div className=" flex w-[24px] justify-center border-x-[1px] border-b-[1px] border-neutral-800 bg-[#c5f7c5] pt-[1px]">
         {linkNum}
       </div>
-      <div className="w-full border-b-[1px] border-solid border-black bg-newsprint-pink p-1 py-0.5 text-left text-[11px] leading-none md:text-[10px] xl:text-[11px]">
+      <div className="w-full border-b-[1px] border-r-[1px] border-solid border-gray-600 bg-newsprint-pink p-1 text-left text-justify text-[11px] leading-none md:text-[10px] xl:text-[11px]">
         {description?.includes('∞∞') ? '' : description}
       </div>
       {link && (
-        <div className="absolute bottom-0 right-0 border-y-[1px] border-l-[1px] border-black bg-link-name px-2 text-[11px] font-bold text-purple-800">
+        <div className="absolute bottom-0 right-0 border-[1px] border-black bg-link-name px-2 text-[10px] font-bold leading-none text-purple-800">
           {link}
         </div>
       )}
@@ -51,10 +51,10 @@ const FileLinks: React.FC<Props> = ({ file, sanityFile, search }) => {
     ['245', '345', '1234', '1235', '1245', '1345', '2345', '12345'],
   ]
   return (
-    <div className="flex w-full flex-row flex-wrap">
+    <div className="grid gap-1 xs:grid-cols-2 md:grid-cols-4">
       {range(1, 5).map((quadrant) => (
-        <div className="flex w-full flex-col gap-x-2 border-black sm:w-1/2 md:w-1/4" key={quadrant}>
-          <h3 className="border-x-[1px] border-y-[2px] border-black bg-green-200">Quadrant {quadrant}</h3>
+        <div className="- flex w-full flex-col border-black" key={quadrant}>
+          <h3 className="rounded-t-sm border-[1px] border-gray-500 bg-green-200">Quadrant {quadrant}</h3>
           {range(1, 9).map((linkNum) => {
             const filemakerSuffix = descriptionRows[quadrant - 1][linkNum - 1]
             const filemakerDescription = file && file[`sm${filemakerSuffix}`]
