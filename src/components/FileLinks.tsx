49e113e74144963a9a73a99c30bfc1bd9b571753
Shalin Scupham <shalin.scupham@gmail.com>
2024-01-04 20:05:39 -0500

    Null guard against linklessness

--- src/components/FileLinks.tsx
+++ src/components/FileLinks.tsx
@@ -19,7 +19,7 @@ const LinkGrid = ({
   link: string
 }) => {
   return (
-    <a className={`relative flex h-[51px] ${link.trim() && 'hover:cursor-pointer'}`} onClick={onClick}>
+    <a className={`relative flex h-[51px] ${link?.trim() && 'hover:cursor-pointer'}`} onClick={onClick}>
       <div className=" flex w-[24px] justify-center border-x-[1px] border-b-[1px] border-neutral-800 bg-[#c5f7c5] pt-[1px]">
         {linkNum}
       </div>
