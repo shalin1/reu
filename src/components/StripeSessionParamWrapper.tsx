581282f74af0872ce498b5b8ade04788a0bb8910
Sean Marzug-McCarthy <samdec11@gmail.com>
2024-01-29 04:57:31 -0500

    Steps toward funneling unsubbed folk through checkout

--- src/components/StripeSessionParamWrapper.tsx
+++ src/components/StripeSessionParamWrapper.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Navigate, Outlet, useSearchParams } from 'react-router-dom'
 
-const StateParamWrapper = () => {
+const StripeSessionParamWrapper = () => {
   const [search] = useSearchParams()
   const stripeSessionParam = search.get('session_id')
 
   return stripeSessionParam ? <Outlet /> : <Navigate to="/" replace />
 }
 
-export default StateParamWrapper
+export default StripeSessionParamWrapper
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
 import ErrorPage from './ErrorPage'
 import Auth0ProviderLayout from './components/Auth0ProviderLayout'
 import StripeSessionParamWrapper from './components/StripeSessionParamWrapper'
 import Checkout from './pages/Checkout'
+import ReunionSession from './pages/ReunionSession'
+import Splash from './pages/Splash'
 import Success from './pages/Success'
 import './index.css'
 
@@ -15,7 +16,8 @@ const queryClient = new QueryClient()
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Auth0ProviderLayout />} errorElement={<ErrorPage />}>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Splash />} />
+      <Route path="/session" element={<ReunionSession />} />
       <Route path="/order/checkout" element={<Checkout />} />
       <Route element={<StripeSessionParamWrapper />}>
         <Route path="/order/success" element={<Success />} />
