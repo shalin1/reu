f0615da0a9df57b5fbe3b125cf98a067d45f6271
Sean Marzug-McCarthy <samdec11@gmail.com>
2024-01-30 14:10:51 -0500

    Redirect to checkout if no active subscription

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Stripe from 'stripe'
 
 export const getStripeSessionData = async (sessionId: string) => {
   const response = await axios.get(`/api/stripe-session-details/${sessionId}`)
@@ -6,10 +7,6 @@ export const getStripeSessionData = async (sessionId: string) => {
 }
 
 export const getStripeSubscriptionStatus = async (stripeCustomerId: string) => {
-  if (stripeCustomerId) {
-    const response = await axios.get(`/api/stripe-subscription-details/${stripeCustomerId}`)
-    return response.data
-  } else {
-    return {}
-  }
+  const response = await axios.get(`/api/stripe-subscription-details/${stripeCustomerId}`)
+  return response.data.subscriptions.some((subscription: Stripe.Subscription) => subscription.status === 'active')
 }
