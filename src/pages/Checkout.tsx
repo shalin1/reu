43557668ee04ee04be0728215aad0a8029f843f9
Sean Marzug-McCarthy <samdec11@gmail.com>
2024-02-02 17:09:28 -0500

    Little bit of cleanup

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 
-const Checkout = () => (
-  <div className="text-left">
-    <p>
-      You must have an active subscription in order to use the Reunion web tool. Please choose one of the two options
-      below and click the Checkout button to continue to billing. You will be returned to this site after finalizing
-      your order.
-    </p>
-    <form action="/api/create-checkout-session" method="POST">
-      <input type="radio" id="monthly" name="priceId" value="price_0OcWYkO0ta6i0fcmKCPElToa" />
-      <label htmlFor="monthly">$40/month</label>
-      <input type="radio" id="yearly" name="priceId" value="price_0OcWYkO0ta6i0fcmYp0ifF0o" />
-      <label htmlFor="yearly">$450/year</label>
-      <br />
-      <button type="submit" className="btn-primary">
-        Checkout
-      </button>
-    </form>
-  </div>
-)
+type SubscriptionOption = {
+  name: string,
+  priceId: string,
+  label: string
+}
+const subscriptionOptions = [
+  { name: 'monthly', priceId: 'price_0OcWYkO0ta6i0fcmKCPElToa', label: '$40/month' },
+  { name: 'yearly', priceId: 'price_0OcWYkO0ta6i0fcmYp0ifF0o', label: '$450/year' }
+]
+
+const Checkout = () => {
+  const [selectedPriceId, setSelectedPriceId] = useState('')
+  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedPriceId(event.target.value)
+  }
+  const optionDivClass = (option: SubscriptionOption) => classNames(
+    'border-2', 'rounded-full', 'p-2', {
+      'border-red-500': selectedPriceId === option.priceId,
+      'border-gray-300': selectedPriceId !== option.priceId,
+      'hover:border-red-300': selectedPriceId !== option.priceId
+    }
+  )
+
+  return (
+    <div>
+      <p>
+        You must have an active subscription in order to use the Reunion web tool. Please choose one of the two options
+        below and click the Checkout button to continue to billing. You will be returned to this site after finalizing
+        your order.
+      </p>
+      <form action="/api/create-checkout-session" method="POST">
+        <div className="flex gap-5">
+          {
+            subscriptionOptions.map((option) => (
+              <div key={option.name} className={optionDivClass(option)}>
+                <input
+                  type="radio"
+                  id={option.name}
+                  name="priceId"
+                  value={option.priceId}
+                  className="sr-only"
+                  onChange={handlePriceChange}
+                  checked={selectedPriceId === option.priceId}
+                />
+                <label htmlFor={option.name}>{option.label}</label>
+              </div>
+            ))
+          }
+        </div>
+        <br />
+        <button type="submit" className="btn-primary">
+          Checkout
+        </button>
+      </form>
+    </div>
+  )
+}
 
 export default Checkout
