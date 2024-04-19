c1bde9ee351e34795ed2030f42b1c3c6b1114946
Shalin Scupham <shalin.scupham@gmail.com>
2024-04-19 16:54:44 -0400

    betcha this works in prod

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5252/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
   },
 })
