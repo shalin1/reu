540464f741f5ebf8a0323be90caacc88e0eabe48
Shalin Scupham <shalin.scupham@gmail.com>
2024-04-19 14:23:15 -0400

    re add api

--- src/api.ts
+++ src/api.ts
@@ -3,11 +3,11 @@ import Stripe from 'stripe'
 
 const serverUrl = import.meta.env.SERVER_URL
 export const getStripeSessionData = async (sessionId: string) => {
-  const response = await axios.get(`${serverUrl}/stripe-session-details/${sessionId}`)
+  const response = await axios.get(`api/stripe-session-details/${sessionId}`)
   return response.data
 }
 
 export const getHasActiveStripeSubscription = async (stripeCustomerId: string) => {
-  const response = await axios.get(`${serverUrl}/stripe-subscription-details/${stripeCustomerId}`)
+  const response = await axios.get(`api/stripe-subscription-details/${stripeCustomerId}`)
   return response.data.subscriptions.some((subscription: Stripe.Subscription) => subscription.status === 'active')
 }
