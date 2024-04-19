9adbd92e22705f6a595e12fba90b69e8e43a31ff
Shalin Scupham <shalin.scupham@gmail.com>
2024-04-19 14:45:58 -0400

    env&?

--- src/data/sanityClient.ts
+++ src/data/sanityClient.ts
@@ -7,9 +7,8 @@ const config: ClientConfig = {
   apiVersion: '2023-06-16', // use a UTC date string
   dataset: 'production', // this is from those question during 'sanity init'
   useCdn: false, // `false` if you want to ensure fresh data
-  projectId: 'rxwn78ar',
-  token:
-    'sk10ftX6GJZXbQFbqdB8rrULDlDcyWM6NagGI2J1fGJIx5A52NBPGTmbXP3QoCwZl2vZ67emHsC235evQ6WQfAwq9VvvtjYFmnSzdomq8olIxYFKCWdH50MfOooabvcru8HGEqs9pSdxWBU41u8ZIxh7hMC46nR1FPTsdO8MaF6feaH1plmZ',
+  projectId,
+  token,
 }
 
 const sanityClient = createClient(config)
