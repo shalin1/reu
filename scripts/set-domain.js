48a813187c3a4019b3f735f0fe4ebd1a2b13c255
Sean Marzug-McCarthy <samdec11@gmail.com>
2024-05-03 14:44:51 -0400

    Take 2

--- scripts/set-domain.js
+++ scripts/set-domain.js
@@ -4,16 +4,13 @@ const fs = require('fs')
 const deployUrl = process.env.DEPLOY_PRIME_URL
 const context = process.env.CONTEXT
 
-console.log('BOOGA')
-console.log(deployUrl)
-
 // default ot existing env var
-let clientDomain = process.env.VITE_CLIENT_DOMAIN
+let clientDomain = process.env.CLIENT_DOMAIN || process.env.VITE_CLIENT_DOMAIN
 
 if (context === 'deploy-preview') {
   clientDomain = deployUrl
 }
 
 // Write the environment variable to a file that can be included in the build
-const content = `VITE_CLIENT_DOMAIN=${clientDomain}\n`
+const content = `CLIENT_DOMAIN=${clientDomain}\nVITE_CLIENT_DOMAIN=${clientDomain}\n`
 fs.appendFileSync('./.env.production', content)
