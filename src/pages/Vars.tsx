1c8d59035da33493d7164751e2127cffbcd5ccd9
Sean Marzug-McCarthy <samdec11@gmail.com>
2024-05-04 12:27:22 -0400

    Clean up vars page

--- src/pages/Vars.tsx
+++ src/pages/Vars.tsx
@@ -12,22 +12,14 @@ const Vars = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>VITE_AUTH0_DOMAIN</td>
-          <td>{VITE_AUTH0_DOMAIN}</td>
-        </tr>
-        <tr>
-          <td>VITE_CLIENT_DOMAIN</td>
-          <td>{VITE_CLIENT_DOMAIN}</td>
-        </tr>
-        <tr>
-          <td>VITE_DISABLE_PAYWALL</td>
-          <td>{VITE_DISABLE_PAYWALL}</td>
-        </tr>
-        <tr>
-          <td>VITE_SANITY_STUDIO_PROJECT_ID</td>
-          <td>{VITE_SANITY_STUDIO_PROJECT_ID}</td>
-        </tr>
+        {
+          Object.entries({ VITE_AUTH0_DOMAIN, VITE_CLIENT_DOMAIN, VITE_DISABLE_PAYWALL, VITE_SANITY_STUDIO_PROJECT_ID }).map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))
+        }
       </tbody>
     </table>
   )
