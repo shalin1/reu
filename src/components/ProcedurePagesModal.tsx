43f6ee48daff6e491df564d593f1018423ec7bae
Shalin Scupham <shalin.scupham@gmail.com>
2024-05-13 08:54:21 -0400

    Refactor in to smaller chunks

--- src/components/ProcedurePagesModal.tsx
+++ src/components/ProcedurePagesModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { useState } from 'react'
 
 // Import images
 import europe from '/src/images/europe.jpg'
@@ -11,7 +11,7 @@ import oceania from '/src/images/oceania.jpg'
 import canada from '/src/images/canada.jpg'
 import southAmerica from '/src/images/south america.jpg'
 
-const images = [world, unitedStates, southAmerica, canada, europe, middleEast, asia, africa, oceania]
+const mapImages = [world, unitedStates, southAmerica, canada, europe, middleEast, asia, africa, oceania]
 
 interface Props {
   hidden: boolean
@@ -21,6 +21,8 @@ interface Props {
 
 const ProcedurePagesModal: React.FC<Props> = ({ hidden, showModal, setShowModal }) => {
   const [currentImage, setCurrentImage] = useState(0)
+
+  const images = mapImages
 
   const nextImage = () => {
     setCurrentImage((prev) => (prev + 1) % images.length)
@@ -55,35 +57,29 @@ const ProcedurePagesModal: React.FC<Props> = ({ hidden, showModal, setShowModal 
     </button>
   )
 
-  const ModalContainer: React.FC<{ children: ReactElement }> = ({ children }) => (
-    <>
-      <div className="fixed inset-0 z-50 flex items-center justify-center p-4 outline-none focus:outline-none">
-        <div className="relative h-full w-full overflow-hidden rounded-lg bg-white shadow-lg">
-          <div className="flex items-start justify-between border-b p-5">
-            <h3 className="text-2xl font-semibold">Procedure Pages</h3>
-            <button
-              className="text-3xl leading-none text-black opacity-50 outline-none focus:outline-none"
-              onClick={() => setShowModal(false)}
-            >
-              <span className="text-black opacity-50">×</span>
-            </button>
-          </div>
-          {children}
-        </div>
-      </div>
-      <div className="fixed inset-0 z-40 bg-black opacity-50"></div>
-    </>
+  const ModalBackdrop = () => <div className="fixed inset-0 z-40 bg-black opacity-50" />
+
+  const ModalHeader = () => (
+    <div className="flex justify-between border-b p-5">
+      <h3 className="text-2xl font-semibold">Procedure Pages</h3>
+      <button
+        className="text-3xl leading-none text-black opacity-50 outline-none focus:outline-none"
+        onClick={() => setShowModal(false)}
+      >
+        <span className="text-black opacity-50">×</span>
+      </button>
+    </div>
   )
 
-  const ModalContent = () => (
-    <div className="flex h-[90vh] items-center justify-between p-6">
+  const ModalBody = () => (
+    <div className="flex items-center justify-between p-2">
       <button
         onClick={prevImage}
         className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
       >
         &#10094;
       </button>
-      <img src={images[currentImage]} alt="Procedure Map" className="object-fit max-h-[80vh] max-w-[70vw]" />
+      <ModalContent />
       <button
         onClick={nextImage}
         className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
@@ -93,14 +89,27 @@ const ProcedurePagesModal: React.FC<Props> = ({ hidden, showModal, setShowModal 
     </div>
   )
 
+  const ModalContainer = () => (
+    <>
+      <div className="fixed inset-0 z-50 flex p-4 outline-none focus:outline-none">
+        <div className="flex h-full w-full flex-col overflow-hidden rounded-lg bg-white shadow-lg">
+          <ModalHeader />
+          <ModalBody />
+        </div>
+      </div>
+      <ModalBackdrop />
+    </>
+  )
+
+  const ModalContent = () => {
+    const src = images[currentImage]
+    return <img src={src} alt="img" className="max-h- h-fit w-fit" />
+  }
+
   return (
     <>
       {!hidden && <ModalButton />}
-      {showModal ? (
-        <ModalContainer>
-          <ModalContent />
-        </ModalContainer>
-      ) : null}
+      {showModal ? <ModalContainer /> : null}
     </>
   )
 }
