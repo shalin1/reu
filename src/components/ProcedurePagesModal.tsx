0208f1940ef7aa5d666f1b725835ecd3d75ed320
Shalin Scupham <shalin.scupham@gmail.com>
2024-05-12 22:11:58 -0400

    Refactor: extract components from modal

--- src/components/ProcedurePagesModal.tsx
+++ src/components/ProcedurePagesModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ReactElement, useState } from 'react'
 
 // Import images
 import europe from '/src/images/europe.jpg'
@@ -49,46 +49,57 @@ const ProcedurePagesModal: React.FC<Props> = ({ hidden, showModal, setShowModal 
     }
   }, [showModal])
 
+  const ModalButton = () => (
+    <button className="btn-primary" onClick={() => setShowModal(true)}>
+      Procedure Pages
+    </button>
+  )
+
+  const ModalContainer: React.FC<{ children: ReactElement }> = ({ children }) => (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center p-4 outline-none focus:outline-none">
+        <div className="relative h-full w-full overflow-hidden rounded-lg bg-white shadow-lg">
+          <div className="flex items-start justify-between border-b p-5">
+            <h3 className="text-2xl font-semibold">Procedure Pages</h3>
+            <button
+              className="text-3xl leading-none text-black opacity-50 outline-none focus:outline-none"
+              onClick={() => setShowModal(false)}
+            >
+              <span className="text-black opacity-50">×</span>
+            </button>
+          </div>
+          {children}
+        </div>
+      </div>
+      <div className="fixed inset-0 z-40 bg-black opacity-50"></div>
+    </>
+  )
+
+  const ModalContent = () => (
+    <div className="flex h-[90vh] items-center justify-between p-6">
+      <button
+        onClick={prevImage}
+        className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
+      >
+        &#10094;
+      </button>
+      <img src={images[currentImage]} alt="Procedure Map" className="object-fit max-h-[80vh] max-w-[70vw]" />
+      <button
+        onClick={nextImage}
+        className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
+      >
+        &#10095;
+      </button>
+    </div>
+  )
+
   return (
     <>
-      {!hidden && (
-        <button className="btn-primary" onClick={() => setShowModal(true)}>
-          Procedure Pages
-        </button>
-      )}
+      {!hidden && <ModalButton />}
       {showModal ? (
-        <>
-          <div className="fixed inset-0 z-50 flex items-center justify-center p-4 outline-none focus:outline-none">
-            <div className="relative h-full w-full overflow-hidden rounded-lg bg-white shadow-lg">
-              <div className="flex items-start justify-between border-b p-5">
-                <h3 className="text-2xl font-semibold">Procedure Pages</h3>
-                <button
-                  className="text-3xl leading-none text-black opacity-50 outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className="text-black opacity-50">×</span>
-                </button>
-              </div>
-
-              <div className="flex h-[90vh] items-center justify-between p-6">
-                <button
-                  onClick={prevImage}
-                  className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
-                >
-                  &#10094;
-                </button>
-                <img src={images[currentImage]} alt="Procedure Map" className="object-fit max-h-[80vh] max-w-[70vw]" />
-                <button
-                  onClick={nextImage}
-                  className="h-[100px] w-[70px] rounded-full bg-violet-400 p-3 text-xl text-white hover:bg-blue-700 focus:outline-none"
-                >
-                  &#10095;
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="fixed inset-0 z-40 bg-black opacity-50"></div>
-        </>
+        <ModalContainer>
+          <ModalContent />
+        </ModalContainer>
       ) : null}
     </>
   )
