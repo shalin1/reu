4b5633129221db9e5e7e11536c6452a67eb59d0c
Shalin Scupham <shalin.scupham@gmail.com>
2024-06-14 21:09:14 -0400

    Add sign up button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,10 +4,26 @@ import { useAuth0 } from '@auth0/auth0-react'
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
 
+  const signUp = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/profile',
+      },
+      authorizationParams: {
+        screen_hint: 'signup',
+      },
+    })
+  }
+
   return (
-    <button className="btn-primary" onClick={() => loginWithRedirect()}>
-      Log in to continue
-    </button>
+    <>
+      <button className="btn-primary" onClick={() => loginWithRedirect()}>
+        Log in
+      </button>
+      <button className="btn-primary" onClick={() => signUp()}>
+        Sign up
+      </button>
+    </>
   )
 }
 
