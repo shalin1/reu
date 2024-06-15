e41c57f882d2858071f166e1f7cfde3dc22631a3
Shalin Scupham <shalin.scupham@gmail.com>
2024-06-14 20:58:04 -0400

    Only one option

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -3,14 +3,11 @@ import classNames from 'classnames'
 import useAuth0UserWithSanity from '../hooks/useAuth0UserWithSanity'
 
 type SubscriptionOption = {
-  name: string,
-  priceId: string,
+  name: string
+  priceId: string
   label: string
 }
-const subscriptionOptions = [
-  { name: 'monthly', priceId: 'price_0OcWYkO0ta6i0fcmKCPElToa', label: '$40/month' },
-  { name: 'yearly', priceId: 'price_0OcWYkO0ta6i0fcmYp0ifF0o', label: '$450/year' }
-]
+const subscriptionOptions = [{ name: 'yearly', priceId: 'price_0OcWYkO0ta6i0fcmYp0ifF0o', label: '$150/year' }]
 
 const Checkout = () => {
   const [selectedPriceId, setSelectedPriceId] = useState('')
@@ -19,12 +16,12 @@ const Checkout = () => {
     setSelectedPriceId(event.target.value)
   }
   const { sanityUser } = useAuth0UserWithSanity()
-  const optionDivClass = (option: SubscriptionOption) => classNames(
-    'border-2', 'rounded-full', 'p-2', {
-    'border-red-500': selectedPriceId === option.priceId,
-    'border-gray-300': selectedPriceId !== option.priceId,
-    'hover:border-red-300': selectedPriceId !== option.priceId
-  })
+  const optionDivClass = (option: SubscriptionOption) =>
+    classNames('border-2', 'rounded-full', 'p-2', {
+      'border-red-500': selectedPriceId === option.priceId,
+      'border-gray-300': selectedPriceId !== option.priceId,
+      'hover:border-red-300': selectedPriceId !== option.priceId,
+    })
   useEffect(() => {
     if (sanityUser && sanityUser.stripeCustomerId) {
       setStripeCustomerId(sanityUser.stripeCustomerId)
@@ -34,28 +31,26 @@ const Checkout = () => {
   return (
     <div>
       <p>
-        You must have an active subscription in order to use the Reunion web tool. Please choose one of the two options
+        You must have an active subscription in order to use the Reunion web tool. Please choose one of the options
         below and click the Checkout button to continue to billing. You will be returned to this site after finalizing
         your order.
       </p>
       <form action="/api/create-checkout-session" method="POST">
-        <div className="flex gap-5 justify-center my-3">
-          {
-            subscriptionOptions.map((option) => (
-              <div key={option.name} className={optionDivClass(option)}>
-                <input
-                  type="radio"
-                  id={option.name}
-                  name="priceId"
-                  value={option.priceId}
-                  className="sr-only"
-                  onChange={handlePriceChange}
-                  checked={selectedPriceId === option.priceId}
-                />
-                <label htmlFor={option.name}>{option.label}</label>
-              </div>
-            ))
-          }
+        <div className="my-3 flex justify-center gap-5">
+          {subscriptionOptions.map((option) => (
+            <div key={option.name} className={optionDivClass(option)}>
+              <input
+                type="radio"
+                id={option.name}
+                name="priceId"
+                value={option.priceId}
+                className="sr-only"
+                onChange={handlePriceChange}
+                checked={selectedPriceId === option.priceId}
+              />
+              <label htmlFor={option.name}>{option.label}</label>
+            </div>
+          ))}
         </div>
         <input type="hidden" id="stripe-customer-id" name="stripeCustomerId" value={stripeCustomerId} />
         <button type="submit" className="btn-primary">
